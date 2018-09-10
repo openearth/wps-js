7ca5a247ab98cb72e6ed2f7c7a1775d8afda15e5
Fedor Baart <fedor.baart@deltares.nl>
2018-09-10 15:46:44 +0200

    add output request

--- src/web/demo.js
+++ src/web/demo.js
@@ -110,7 +110,9 @@ $(document).ready(function() {
     var responseFormat = 'document';
     var executionMode = 'sync';
     var lineage = false;
-    var inputs = []
+    var jsonInput = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[3.2299804687499996,51.52925135518991],[3.427734375,51.172455303299],[4.3011474609375,51.26535213392538],[4.29840087890625,51.42832700421254],[3.6090087890625004,51.515579783755925],[3.7628173828124996,51.67255514839674],[3.93310546875,51.80691653515817],[4.04296875,51.80691653515817],[4.119873046875,51.87309959004367],[4.42474365234375,51.82559340146247],[4.29290771484375,52.05924589011585],[4.64447021484375,52.399067302933304],[4.87518310546875,52.826001860971836],[5.438232421875,53.108865487028616],[5.847473144531249,53.32759237756109],[6.30615234375,53.337433437129675],[6.712646484375,53.28492154619624],[7.086181640625,53.1928702436326],[7.283935546874999,53.199451902831555],[7.218017578125001,53.44880683542759],[6.8994140625,53.75520681580145],[5.44921875,65.25670649344259],[-9.4921875,63.39152174400882],[0.85693359375,52.01869808104436],[3.2299804687499996,51.52925135518991]]]}}]};
+
+    var inputs = [];
     var inputGenerator = new InputGenerator();
     var input = inputGenerator.createComplexDataInput_wps_1_0_and_2_0(
       'json_input',
@@ -118,10 +120,28 @@ $(document).ready(function() {
       undefined,
       undefined,
       false,
-      JSON.stringify({a: 3})
+      JSON.stringify(jsonInput)
     );
     inputs.push(input);
+
     var outputs = [];
+
+    var outputGenerator = new OutputGenerator();
+    var identifier = 'json_output';
+    var asReference = false;
+    var mimeType = 'application/json';
+    var complexOutput = outputGenerator.createComplexOutput_WPS_1_0(
+      identifier,
+      mimeType,
+      undefined,
+			undefined,
+      undefined,
+      asReference,
+      'title',
+      'abstract'
+    );
+    outputs.push(complexOutput);
+
     wps.execute(
       callbackFunction,
       processIdentifier,
